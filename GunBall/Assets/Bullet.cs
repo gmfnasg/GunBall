62012d7a3d94612cb6884020d8219d455f8ab49e
gmfnasg <gpx_mark_farng@hotamil.com>
2016-02-03 21:11:10 +0800

    BaseGameRoom

---
+++ GunBall/Assets/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+    
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, Vector3.zero)> 100)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ GunBall/Assets/ShootBullet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootBullet : MonoBehaviour {
+    public GameObject BulletBase;
+    public float Speed = 1;
+
+	// Use this for initialization
+	void Start () {
+        if (BulletBase == null)
+        {
+            BulletBase = GameObject.Find("Bullet");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.X))
+        {
+            if (BulletBase != null)
+            {
+                GameObject newBullet = GameObject.Instantiate(BulletBase, transform.position, transform.rotation) as GameObject;
+                Rigidbody rb = newBullet.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddForce(transform.forward.normalized* Speed);
+                }
+            }
+        }
+	}
+}
